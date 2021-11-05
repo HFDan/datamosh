b29b51c97b3867a1cf7967c436663e0a05289e17
HFDan <58553089+HFDan@users.noreply.github.com>
2021-11-05 18:06:12 +0200

    Adopted new argument parsing method

--- include/datamosh/arg.h
+++ include/datamosh/arg.h
@@ -4,22 +4,28 @@
 /**
  * @brief Get option by argument
  *
- * @param arg The argument to look for
+ * @param short_arg The argument to look for (short)
+ * @param long_arg The argument to look for (long)
  * @param argc The argument count
  * @param argv The argument vector
+ * @param required Not implemented
  * @return char* Will return NULL if argument does not exist
  */
 const char* get_option(
 	const char* short_arg, const char* long_arg, const int argc,
-	const char** argv);
+	const char** argv, bool required);
 
 /**
  * @brief Does option exist?
  *
- * @param arg The argument to look for
+ * @param short_arg The argument to look for (short)
+ * @param long_arg The argument to look for (long)
  * @param argc The argument count
  * @param argv The argument vector
+ * @param required Not implemented
  * @return true If the argument is found
  * @return false If the argument is not found
  */
-bool option_exists(const char* arg, int argc, const char** argv);
+bool option_exists(
+	const char* short_arg, const char* long_arg, int argc, const char** argv,
+	bool required);
--- src/datamosh/arg.c
+++ src/datamosh/arg.c
@@ -1,12 +1,30 @@
+#include <errno.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+/**
+ * @brief This is an internal function, do not expose to a header file
+ *
+ * @param a
+ * @param b
+ * @return int
+ */
+int strcmp_safe(const char* a, const char* b) {
+	if (a == NULL || b == NULL) {
+		return 1;
+	}
+
+	return strcmp(a, b);
+}
 
 const char* get_option(
 	const char* short_arg, const char* long_arg, const int argc,
-	const char** argv) {
+	const char** argv, bool required) {
 	for (int i = 1; i < argc; i++) {
-		if ((strcmp(argv[i], short_arg) == 0 ||
-			 strcmp(argv[i], long_arg) == 0) &&
+		if ((strcmp_safe(argv[i], short_arg) == 0 ||
+			 strcmp_safe(argv[i], long_arg) == 0) &&
 			i != argc) {
 			if (argv[i + 1][0] != '-') {
 				return argv[i + 1];
@@ -14,15 +32,30 @@ const char* get_option(
 		}
 	}
 
+	if (required) {
+		fprintf(
+			stderr, "Required argument not given: %s\n",
+			(long_arg != NULL) ? long_arg : short_arg);
+		exit(EXIT_FAILURE);
+	}
+
 	return NULL;
 }
 
 bool option_exists(
 	const char* short_arg, const char* long_arg, const int argc,
-	const char** argv) {
+	const char** argv, bool required) {
 	for (int i = 1; i < argc; i++) {
-		if (strcmp(argv[i], short_arg) == 0 || strcmp(argv[i], long_arg) == 0)
+		if (strcmp_safe(argv[i], short_arg) == 0 ||
+			strcmp_safe(argv[i], long_arg) == 0)
 			return true;
+	}
+
+	if (required) {
+		fprintf(
+			stderr, "Required argument not given: %s\n",
+			(long_arg != NULL) ? long_arg : short_arg);
+		exit(EXIT_FAILURE);
 	}
 
 	return false;
